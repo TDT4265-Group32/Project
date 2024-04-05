93a9c8860ed3b095ad743eec732bb2a8bd6de24b
Christian Le <cl@stud.ntnu.no>
2024-04-05 19:58:50 +0200

    fix main.py predict part

--- src/main.py
+++ src/main.py
@@ -11,17 +11,14 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = YOLO('models/pretrained/yolov8n.pt')
 # We can also use our own custom model: YOLO('path/to/model.pt')
 
-partition_dataset(dataset_dir='datasets/NAPLab-LiDAR', force_repartition=True)
-result = model.train(data='data/NAPLab-LiDAR.yaml', epochs=100, imgsz=640, device=device)
+partition_dataset(dataset_dir='datasets/NAPLab-LiDAR', force_repartition=False)
+result = model.train(data='data/NAPLab-LiDAR.yaml', epochs=10, imgsz=640, device=device)
 
 # Since the model is already trained, we can use the model to predict (no input data is required as it is already trained)
 metrics = model.val()
 
 # Test prediction on an image
-test_results = model.test()
-
-if os.path.exists("models/NAPLab-Lidar") == False:
-    os.makedirs("models/NAPLab-Lidar")
+test_results = model.predict('datasets/NAPLab-LiDAR/images/test/')
 
 # Export trained model
 model.export()
