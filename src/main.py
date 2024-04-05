c4fccb079a7d8a98d11aa96101affc918fb84938
Christian Le <cl@stud.ntnu.no>
2024-04-05 19:38:18 +0200

    make main.py work properly

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from ultralytics import YOLO
 from partition_dataset import partition_dataset
 import os
 import torch
+import random
+random.seed(0)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -9,7 +11,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = YOLO('models/pretrained/yolov8n.pt')
 # We can also use our own custom model: YOLO('path/to/model.pt')
 
-partition_dataset(dataset_dir='datasets/NAPLab-LiDAR')
+partition_dataset(dataset_dir='datasets/NAPLab-LiDAR', force_repartition=True)
 result = model.train(data='data/NAPLab-LiDAR.yaml', epochs=100, imgsz=640, device=device)
 
 # Since the model is already trained, we can use the model to predict (no input data is required as it is already trained)
