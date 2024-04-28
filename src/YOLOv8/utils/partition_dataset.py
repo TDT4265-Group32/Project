77007df3d15bf426ec337d89bccb45039eedf833
Christian Le <cl@stud.ntnu.no>
2024-04-28 15:13:54 +0200

    update partition dataset to work properly

--- src/YOLOv8/utils/partition_dataset.py
+++ src/YOLOv8/utils/partition_dataset.py
@@ -9,50 +9,57 @@ import pandas as pd
 import collections
 from sklearn.model_selection import KFold
 
-def partition_dataset(dataset, train_ratio=0.8, val_ratio=0.2):
-    print(f'Partitioning dataset in {dataset} into train, val, and test subsets...\n')
-    dataset_path = f'datasets/{dataset}'
-    image_dir = os.path.join(dataset_path, 'images')
-    label_dir = os.path.join(dataset_path, 'labels')
-
-    assert train_ratio + val_ratio == 1
-
-    all_images = [img for img in os.listdir(image_dir) if img.endswith('.PNG')]
-    all_labels = [label for label in os.listdir(label_dir) if label.endswith('.txt')]
-    
-    assert len(all_images) == len(all_labels), 'Number of images and labels do not match'
-
-    random.shuffle(all_images)
-
-    train_size = int(len(all_images) * train_ratio)
-    val_size = int(len(all_images) * val_ratio)
-
-    train_images = all_images[:train_size]
-    val_images = all_images[train_size:train_size + val_size]
-    
-    train_labels = [img.replace('.PNG', '.txt') for img in train_images]
-    val_labels = [img.replace('.PNG', '.txt') for img in val_images]
-
-    def copy_files(images, labels, subset):
-        img_subset_dir = os.path.join(image_dir, subset)
-        lbl_subset_dir = os.path.join(label_dir, subset)
-
+def _copy_files(images,
+                labels,
+                subset,
+                dataset_path):
+        img_subset_dir = os.path.join(dataset_path, 'images', subset)
+        lbl_subset_dir = os.path.join(dataset_path, 'labels', subset)
+        
         print(f'Copying {subset} images into {img_subset_dir}...')
         if os.path.exists(img_subset_dir):
             shutil.rmtree(img_subset_dir)
         os.makedirs(img_subset_dir, exist_ok=True)
         for image in tqdm(images, desc=f'Copying {subset} images'):
-            shutil.copy(os.path.join(image_dir, image), os.path.join(img_subset_dir, image))
+            shutil.copy(image, img_subset_dir)
 
         print(f'Copying {subset} labels into {lbl_subset_dir}...')
         if os.path.exists(lbl_subset_dir):
             shutil.rmtree(lbl_subset_dir)
         os.makedirs(lbl_subset_dir, exist_ok=True)
         for label in tqdm(labels, desc=f'Copying {subset} labels'):
-            shutil.copy(os.path.join(label_dir, label), os.path.join(lbl_subset_dir, label))
+            shutil.copy(label, lbl_subset_dir)
 
-    copy_files(train_images, train_labels, 'train')
-    copy_files(val_images, val_labels, 'val')
+def partition_dataset(dataset: str,
+                      train_ratio: float = 0.8):
+    """Divide a dataset into training, validation, and test subsets.
+
+    Args:
+        dataset (str): Which dataset to partition
+        train_ratio (float, optional): _description_. Defaults to 0.8, val_ratio is the complement of train_ratio.
+    """
+    print(f'Partitioning dataset in {dataset} into train, val, and test subsets...\n')
+    try:
+        dataset_path = pathlib.Path(f'datasets/{dataset}')
+    except FileNotFoundError:
+        raise Exception(f"Dataset {dataset} not found")
+
+    assert train_ratio < 1, 'Train ratio must be less than 1'
+    assert train_ratio > 0, 'Train ratio must be greater than 0'
+    
+    labels = sorted(dataset_path.glob("labels/*.txt"))
+
+    random.shuffle(labels)
+
+    images = [label.parent.parent / 'images' / (label.stem + '.PNG') for label in labels]
+
+    train_labels = labels[:int(len(labels) * train_ratio)]
+    val_labels = labels[int(len(labels) * train_ratio):]
+    train_images = images[:int(len(images) * train_ratio)]
+    val_images = images[int(len(images) * train_ratio):]
+
+    _copy_files(train_images, train_labels, 'train', dataset_path)
+    _copy_files(val_images, val_labels, 'val', dataset_path)
 
 def partition_video_dataset(dataset: str,
                             num_clips: int,
@@ -104,114 +111,9 @@ def partition_video_dataset(dataset: str,
     val_labels = [label for sublist in val_labels for label in sublist]
     train_imgs = [label.parent.parent / 'images' / (label.stem + '.PNG') for label in train_labels]
     val_imgs = [label.parent.parent / 'images' / (label.stem + '.PNG') for label in val_labels]
-    
-    def copy_files(images, labels, subset):
-        img_subset_dir = os.path.join(dataset_path, 'images', subset)
-        lbl_subset_dir = os.path.join(dataset_path, 'labels', subset)
-        
-        print(f'Copying {subset} images into {img_subset_dir}...')
-        if os.path.exists(img_subset_dir):
-            shutil.rmtree(img_subset_dir)
-        os.makedirs(img_subset_dir, exist_ok=True)
-        for image in tqdm(images, desc=f'Copying {subset} images'):
-            shutil.copy(image, img_subset_dir)
 
-        print(f'Copying {subset} labels into {lbl_subset_dir}...')
-        if os.path.exists(lbl_subset_dir):
-            shutil.rmtree(lbl_subset_dir)
-        os.makedirs(lbl_subset_dir, exist_ok=True)
-        for label in tqdm(labels, desc=f'Copying {subset} labels'):
-            shutil.copy(label, lbl_subset_dir)
-
-    copy_files(train_imgs, train_labels, 'train')
-    copy_files(val_imgs, val_labels, 'val')
-
-    
-
-def kfold_crossval_partition(dataset: str, 
-                             k: int, 
-                             seed: int = 0):
-    """Function to partition dataset into k-folds for cross-validation.
-    Assumes YOLOv8 dataset structure.
-    
-    Args:
-    dataset_dir (str): Path to the dataset directory
-    yaml_dir (str): Path to the YAML file containing the class names
-    k (int): Number of folds
-    seed (int): Random seed for reproducibility
-    
-    """
-
-    # Try to load the dataset and YAML file
-    try:
-        dataset_path = pathlib.Path(f'datasets/{dataset}')
-        yaml_path = f'data/{dataset}.yaml'
-    except FileNotFoundError:
-        raise Exception(f"Dataset {dataset} not found")
-
-    # Extract labels and names from the dataset
-    labels = sorted(dataset_path.glob("labels/*.txt"))
-        
-    with open(yaml_path, 'r', encoding='utf-8') as f:
-        classes = yaml.safe_load(f)['names']
-
-    cls_idx = sorted(classes.keys())
-
-    # Create a DataFrame to store number of a class in each fold
-    indx = [label.stem for label in labels]
-    labels_df = pd.DataFrame([], columns=cls_idx, index=indx)
-
-    for label in labels:
-        lbl_counter = collections.Counter()
-        
-        # Open the label file and count the number of each class in the file
-        with open(label, 'r') as f:
-            lines = f.readlines()
-
-        # Increment the counter for each class in the label file
-        for line in lines:
-            lbl_counter[int(line.split(' ')[0])] += 1
-
-        # Insert the counter into the DataFrame
-        labels_df.loc[label.stem] = lbl_counter
-
-    # Fill NaN values with 0
-    labels_df = labels_df.fillna(0)
-
-    ### K-Fold Splitting ###
-    
-    kf = KFold(n_splits=k, random_state=seed, shuffle=True)
-    
-    kfolds = list(kf.split(labels_df))
-    
-    folds = [f'split_{i}' for i in range(1, k+1)]
-    folds_df = pd.DataFrame(index=indx, columns=folds)
-    
-    for idx, (train, val) in enumerate(kfolds, start=1):
-        folds_df[f'split_{idx}'].loc[labels_df.iloc[train].index] = 'train'
-        folds_df[f'split_{idx}'].loc[labels_df.iloc[val].index] = 'val'
-
-    fold_lbl_distrb = pd.DataFrame(index=folds, columns=cls_idx)
-    
-    for n, (train_idx, val_idx) in enumerate(kfolds, start=1):
-        train_totals = labels_df.iloc[train_idx].sum()
-        val_totals = labels_df.iloc[val_idx].sum()
-        
-        ratio = val_totals / (train_totals + 1E-7)
-        fold_lbl_distrb.loc[f'split_{n}'] = ratio
-        
-    raise NotImplementedError('Function not yet completed')
+    _copy_files(train_imgs, train_labels, 'train', dataset_path)
+    _copy_files(val_imgs, val_labels, 'val', dataset_path)
 
 if __name__ == "__main__":
-    # Temporary test code
-    for i in range(10):
-        partition_video_dataset('NAPLab-LiDAR', 18, 1)
-
-    
-    
-        
-        
-    
-    
-    
-    
+    partition_dataset('NAPLab-LiDAR')
