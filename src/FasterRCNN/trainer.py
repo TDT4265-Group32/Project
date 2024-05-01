f143049904139a80cfac6d3a0f275bbd14c93951
alekskl01 <alekskl@stud.ntnu.no>
2024-05-01 18:27:09 +0200

    switched to different scheduler for now

--- src/FasterRCNN/trainer.py
+++ src/FasterRCNN/trainer.py
@@ -32,11 +32,12 @@ class CustomFasterRCNN(pl.LightningModule):
     
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=self.config.max_lr, momentum=self.config.momentum, weight_decay=self.config.weight_decay)
-        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30,80], gamma=0.1)
+        #lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
         return {
             "optimizer": optimizer,
             "lr_scheduler": lr_scheduler,
-            #"monitor": "Validation_mAP",
+            #"monitor": "Validation_mAP",  # Ensure Validation_mAP is being monitored
         }
 
     def forward(self, x, y=None):
@@ -74,13 +75,14 @@ class CustomFasterRCNN(pl.LightningModule):
     
     def validation_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self.forward(x)
+        y_hat = self.forward(x, y)  # Pass both x and y to forward
         acc = self.acc_fn(y_hat, y)
         loss = giou_loss(y_hat, y)
+        self.log('Validation_mAP', acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         self.log_dict({
-            "val/loss":loss,
+            "val/loss": loss,
             "val/acc": acc
-        },on_epoch=True, on_step=False, prog_bar=True, sync_dist=True)
+        }, on_epoch=True, on_step=False, prog_bar=True, sync_dist=True)
     
     def test_step(self, batch, batch_idx):
         x, y = batch
